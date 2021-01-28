b2a4d6ab9930b22c9f1a43e8ed1acf24cde50b98
pangweiwei <pangweiwei@21epub.com>
2021-01-28 11:35:39 +0800

    fix bug

--- epub/apps/epub_auth/models.py
+++ epub/apps/epub_auth/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext as _
 
 
 class User(AbstractUser):
