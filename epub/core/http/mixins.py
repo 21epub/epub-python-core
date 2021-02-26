d94d99322c79f9134708e44f1c9625e861190365
waen <waen.wj@gmail.com>
2021-02-26 14:47:17 +0800

    修改Results的返回值  使用est_framework.response   Response

--- epub/core/http/mixins.py
+++ epub/core/http/mixins.py
@@ -11,21 +11,20 @@ class APIViewMixin(object):
     对常见异常的统一处理，使用于所有APIView，统一错误格式。
     注意：
         这个Mixin 使用是需要靠左边。
-
     """
     def dispatch(self, request, *args, **kwargs):
         try:
             return super().dispatch(request, *args, **kwargs)
         except ValueError as e:
-            return Results.error_info(str(e), code=400)
+            return Results.error_info(str(e), 400)
         except TypeError as e:
-            return Results.error_info(str(e), code=400)
+            return Results.error_info(str(e), 400)
         except PermissionDenied:
-            return Results.error_info(u'无访问权限', code=403)
+            return Results.error_info(u'无访问权限', 403)
         except Http404 as e:
             return Results.error_info(str(e), 404)
         except Exception as e:
-            return Results.error_info(e, code=500)
+            return Results.error_info(e, 500)
 
 
 class CsrfExemptMixin(object):
@@ -37,3 +36,50 @@ class CsrfExemptMixin(object):
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+
+
+class CreateResponseMixin(Results):
+    def post(self, request, *args, **kwargs):
+        res = super().post(request, *args, **kwargs)
+        if str(res.status_code).startswith("2"):
+            return self.succss_result(res.data)
+        else:
+            return self.error_info(res.data, res.status_code)
+
+class RetrieveResponseMixin(Results):
+    def retrieve(self, request, *args, **kwargs):
+        res = super().retrieve(request, *args, **kwargs)
+        if str(res.status_code).startswith("2"):
+            return self.succss_result(res.data)
+        else:
+            return self.error_info(res.data, res.status_code)
+
+
+class DeleteResponseMixin(Results):
+    def delete(self, request, *args, **kwargs):
+        res = super().delete(request, *args, **kwargs)
+        if str(res.status_code).startswith("2"):
+            return self.succss_result(res.data)
+        else:
+            return self.error_info(res.data, res.status_code)
+
+
+class UpdateResponseMixin(Results):
+    def update(self, request, *args, **kwargs):
+        res = super().update(request, *args, **kwargs)
+        if str(res.status_code).startswith("2"):
+            return self.succss_result(res.data)
+        else:
+            return self.error_info(res.data, res.status_code)
+
+
+class RetrieveUpdateDeleteResponseMixin(
+    RetrieveResponseMixin,
+    UpdateResponseMixin,
+    DeleteResponseMixin,
+):
+    pass
+
+
+class CreateDeleteResponseMixin(CreateResponseMixin, DeleteResponseMixin):
+    pass
--- epub/core/http/response.py
+++ epub/core/http/response.py
@@ -1,6 +1,4 @@
-import json
 
-from django.http import JsonResponse
 from rest_framework.response import Response
 from epub.core.log import logger
 
@@ -27,7 +25,7 @@ class Results(object):
         }
         if status is None:
             status = code
-        return _info
+        return Response(data=_info, status=status)
 
     def succss_result(self, data=None):
         if data is None:
@@ -40,55 +38,4 @@ class Results(object):
             'sum': self.total,
             'results': data,
         }}
-        return _info
-
-class CreateResponseMixin(Results):
-    def post(self, request, *args, **kwargs):
-        res = super().post(request, *args, **kwargs)
-        if str(res.status_code).startswith("2"):
-            res_data = self.succss_result(res.data)
-        else:
-            res_data = self.error_info(res.data, res.status_code)
-        return Response(res_data)
-
-
-class RetrieveResponseMixin(Results):
-    def retrieve(self, request, *args, **kwargs):
-        res = super().retrieve(request, *args, **kwargs)
-        if str(res.status_code).startswith("2"):
-            res_data = self.succss_result(res.data)
-        else:
-            res_data = self.error_info(res.data, res.status_code)
-        return Response(res_data)
-
-
-class DeleteResponseMixin(Results):
-    def delete(self, request, *args, **kwargs):
-        res = super().delete(request, *args, **kwargs)
-        if str(res.status_code).startswith("2"):
-            res_data = self.succss_result(res.data)
-        else:
-            res_data = self.error_info(res.data, res.status_code)
-        return Response(res_data)
-
-
-class UpdateResponseMixin(Results):
-    def update(self, request, *args, **kwargs):
-        res = super().update(request, *args, **kwargs)
-        if str(res.status_code).startswith("2"):
-            res_data = self.succss_result(res.data)
-        else:
-            res_data = self.error_info(res.data, res.status_code)
-        return Response(res_data)
-
-
-class RetrieveUpdateDeleteResponseMixin(
-    RetrieveResponseMixin,
-    UpdateResponseMixin,
-    DeleteResponseMixin,
-):
-    pass
-
-
-class CreateDeleteResponseMixin(CreateResponseMixin, DeleteResponseMixin):
-    pass
+        return Response(data=_info)
