65c85e2e824cbe4bc20fffc9cd64696349f0db3e
liveact <liveact@163.com>
2022-01-06 12:11:15 +0800

    test book create list permission

--- books/tests/test_permission.py
+++ books/tests/test_permission.py
@@ -1,18 +1,58 @@
-from django.contrib.auth import get_user_model
+from unittest.mock import patch
+
 from django.test import TestCase
 
+from books.authentication import User
 from books.models import Book
 from django.urls import reverse
 
 
-User = get_user_model()
-
-
 class TestPermission(TestCase):
 
-    def test_get_book_permission(self):
-        # test_user = User.objects.create_user(username="test")
-        # book = Book.objects.create(title="book", user=test_user)
+    default_perms = []
+    add_perms = []
+
+    is_superuser = False
+    user_id = 1
+    subuser_id = 1
+
+    def get_mock_user(self):
+        return User(
+            id=self.user_id,
+            username="test_permission_user",
+            subuser={
+                "id": self.subuser_id,
+                "perms": self.default_perms + self.add_perms,
+                "is_superuser": self.is_superuser,
+            },
+        )
+
+    @patch("books.authentication.MockUserAuthentication.authenticate")
+    def test_book_create_permission(self, mock_authenticate_user):
+        mock_authenticate_user.return_value = (self.get_mock_user(), "token_xxxx")
+
+        self.assertEqual(Book.objects.count(), 0)
         url = reverse("book:book_list_api", kwargs={"book_type": "h5"})
         res = self.client.post(url)
+        self.assertEqual(res.status_code, 403)
+
+        self.add_perms.append({"code": "h5.create"})
+        mock_authenticate_user.return_value = (self.get_mock_user(), "token_xxxx")
+
+        res = self.client.post(url)
         self.assertEqual(res.status_code, 201)
+        self.assertEqual(Book.objects.count(), 1)
+
+    @patch("books.authentication.MockUserAuthentication.authenticate")
+    def test_book_list_permission(self, mock_authenticate_user):
+        mock_authenticate_user.return_value = (self.get_mock_user(), "token_xxxx")
+
+        url = reverse("book:book_list_api", kwargs={"book_type": "h5"})
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 403)
+
+        self.add_perms.append({"code": "h5.list"})
+        mock_authenticate_user.return_value = (self.get_mock_user(), "token_xxxx")
+
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
