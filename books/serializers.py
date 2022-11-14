1093845a373b6e13d6a950242ec022084208adb7
Yugali <50856440+liveact@users.noreply.github.com>
2022-11-14 18:46:11 +0800

    修复批量修改设置日志时报错

--- books/serializers.py
+++ books/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 from books.models import Book
 
 
+class BookBatchSerializer(serializers.ListSerializer):
+    def update(self, instances, validated_data):
+        for index, obj in enumerate(instances):
+            self.child.update(obj, validated_data[index])
+        return instances
+
+
 class BookListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ["id", "title", "user"]
+        fields = ["id", "title", "user", "folder"]
+
+        list_serializer_class = BookBatchSerializer
